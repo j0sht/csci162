ba6b6dbfb3a52e0612dc2f1cb660bd1d0b2ad319
Joshua Tate <jmtate@icloud.com>
2018-03-05 15:23:51 -0800

    Reading Learn Prolog Now: added kb1

---
+++ prolog/learn-prolog-now/kb1.pl
@@ -0,0 +1,15 @@
+% There are only three basic constructs in Prolog: facts, rules, and queries.
+% A collections of facts and rules is called a knowledge base (or database)
+%  and Prolog programming is all about writing knowledge bases.
+
+% Knowledge base 1 (kb1) is simply a collection of facts.
+% Facts are used to state things that are unconditionally true
+%  of some situation of interest.
+% mia, jody and yolanda are each a woman
+woman(mia).
+woman(jody).
+woman(yolanda).
+% jody plays air guitar
+playsAirGuitar(jody).
+% A party is taking place
+party.
